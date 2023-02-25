33bcb551ce4c40e4b4ed68759c29a85a642b41fa
asahelcode <hoyx0101@gmail.com>
2023-02-25 10:39:06 +0100

    Add Shopping cart screen

---
+++ src/screens/ShoppingCart.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native"
+
+const ShoppingCart = () => {
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default ShoppingCart;
