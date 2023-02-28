25d4cb092cb91d2de745bd1dc6e63b6fdba4611c
asahelcode <hoyx0101@gmail.com>
2023-02-28 21:36:46 +0100

    Add product Reducer to stoer

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { productsSlice } from "./productsSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { products: productsSlice.reducer },
 });
