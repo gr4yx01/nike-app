fd6e7c78ba521ed2c0a594569d8c2dd966d83015
asahelcode <hoyx0101@gmail.com>
2023-02-28 21:10:15 +0100

    Wrap app with provider

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import Navigation from "./src/Navigation";
+import { Provider } from "react-redux";
+import { store } from "./src/store";
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
 }
