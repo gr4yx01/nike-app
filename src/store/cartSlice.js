7fa75e2531e4d79af605bbac79a0cea879869f4f
asahelcode <hoyx0101@gmail.com>
2023-03-01 19:33:06 +0100

    Change name to carts

---
+++ src/store/cartSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+  deliveryPrice: 15,
+  freeDeliveryFrom: 200,
+};
+
+export const cartSlice = createSlice({
+  name: "carts",
+  initialState,
+  reducers: {
+    addCartItem: (state, action) => {
+      const newProduct = action.payload.product;
+
+      state.items.push({ product: newProduct, quantity: 1 });
+    },
+    changeQuantity: (state, action) => {},
+  },
+});
